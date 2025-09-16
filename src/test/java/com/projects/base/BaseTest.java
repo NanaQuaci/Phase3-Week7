494db94ac426d3b37d1c5226fa88deb6f3729953
Nana Quaci <aducollins49@gmail.com>
2025-09-16 20:46:24 +0000

    Fix test errors

--- src/test/java/com/projects/base/BaseTest.java
+++ src/test/java/com/projects/base/BaseTest.java
@@ -4,19 +4,32 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.*;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class BaseTest {
+
     @BeforeAll
     public static void setUp() {
-        // configurable via -D arguments or CI env vars
+        // Configurable via -D arguments or CI env vars
         Configuration.baseUrl = System.getProperty("baseUrl", "https://www.saucedemo.com");
         Configuration.browser = System.getProperty("browser", "chrome");
         Configuration.headless = Boolean.parseBoolean(System.getProperty("headless", "true"));
         Configuration.browserSize = System.getProperty("browserSize", "1366x768");
         Configuration.timeout = Long.parseLong(System.getProperty("timeout", "5000"));
 
-        // If using remote selenium (Selenium Grid / Selenoid), set:
-        // System.setProperty("selenide.remote", "http://selenoid:4444/wd/hub");
+        // Add safe defaults for Docker/GitHub Actions
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--disable-software-rasterizer");
+        options.addArguments("--remote-debugging-port=9222");
+
+        if (Configuration.headless) {
+            options.addArguments("--headless=new");
+        }
+
+        Configuration.browserCapabilities = options;
 
         // Register Allure listener (attaches screenshots/page source on failures)
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide()
